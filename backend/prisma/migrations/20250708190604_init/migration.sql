0d04df65b662b542360bd77c2b53a7c5a49c9da6
Jami H <K4na@iki.fi>
2025-07-08 22:12:07 +0300

    removed unique from database as it doesnt matter with keycloak

--- backend/prisma/migrations/20250708190604_init/migration.sql
+++ backend/prisma/migrations/20250708190604_init/migration.sql
@@ -34,8 +34,5 @@ CREATE TABLE "Workday" (
     CONSTRAINT "Workday_pkey" PRIMARY KEY ("id")
 );
 
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
 -- AddForeignKey
 ALTER TABLE "Workday" ADD CONSTRAINT "Workday_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
